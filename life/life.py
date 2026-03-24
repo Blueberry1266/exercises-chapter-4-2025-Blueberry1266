bd07fb7a4c1308ccae57956c1b336a57f549ae74
Zijun Xu <zijun05xx@126.com>
2026-03-24 13:18:37 +0000

    Version 1.0 Complete all

--- life/life.py
+++ life/life.py
@@ -1,14 +1,15 @@
 import numpy as np
 from matplotlib import pyplot
 from scipy.signal import convolve2d
+from copy import copy
 
-glider = np.array([[0,1,0],[0,0,1],[1,1,1]])
+glider = np.array([[0, 1, 0], [0, 0, 1], [1, 1, 1]])
 
 blinker = np.array([
-[0,0,0],
-[1,1,1],
-[0,0,0]]
-    )
+    [0, 0, 0],
+    [1, 1, 1],
+    [0, 0, 0]]
+        )
 
 glider_gun = np.array([
     [0, 0, 0, 0, 1, 1, 0, 0, 0],
@@ -49,15 +50,17 @@ glider_gun = np.array([
     [0, 0, 1, 1, 0, 0, 0, 0, 0]
 ])
 
+
 class Game:
-    def __init__(game,Size):
+    def __init__(game, Size):
         game.board = np.zeros((Size, Size))
-    
+
     def play(self):
         print("Playing life. Press ctrl + c to stop.")
         pyplot.ion()
         while True:
-            self.move(); self.show()
+            self.move()
+            self.show()
             pyplot.pause(0.0000005)
 
     def move(self):
@@ -66,13 +69,47 @@ class Game:
 
         for i in range(self.board.shape[0]):
             for j in range(self.board.shape[1]):
-                self.board[i,j]=1 if (NeighbourCount[i, j]==3 or (NeighbourCount[i, j]==2 and self.board[i,j])) else 0
+                self.board[i, j] = 1 if (NeighbourCount[i, j] == 3
+                                         or (NeighbourCount[i, j] == 2
+                                             and self.board[i, j])) else 0
 
-    def __setitem__(self, key, value): 
+    def __setitem__(self, key, value):
         self.board[key] = value
 
-
     def show(self):
-        pyplot.clf() 
+        pyplot.clf()
         pyplot.matshow(self.board, fignum=0, cmap='binary')
         pyplot.show()
+
+    def insert(self, pattern, place):
+        height = (len(pattern.grid)-1)//2
+        width = (len(pattern.grid[0])-1)//2
+        x, y = place[0], place[1]
+        self.board[x-height:x+height+1, y-width:y+width+1] = pattern.grid
+
+
+class Pattern:
+    def __init__(self, grid):
+        self.grid = grid
+
+    def flip_vertical(self):
+        new_grid = self.grid[::-1]
+        return Pattern(new_grid)
+
+    def flip_horizontal(self):
+        new_grid = copy(self.grid)
+        length_grid = len(self.grid[1])
+        for j in range(len(self.grid)):
+            for k in range(length_grid):
+                new_grid[j][k] = self.grid[j][length_grid-1-k]
+        return Pattern(new_grid)
+
+    def flip_diag(self):
+        new_grid = np.transpose(self.grid)
+        return Pattern(new_grid)
+
+    def rotate(self, n):
+        res = self
+        for j in range(n):
+            res = res.flip_diag().flip_vertical()
+        return res
